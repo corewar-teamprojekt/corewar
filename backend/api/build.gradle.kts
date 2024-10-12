45394a102bdd5c601d2e2d49731604f9f83b40f8
flamion <contact@flamion.dev>
2024-10-12 16:39:09 +0200

    Add default ktor project setup

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,5 +1,13 @@
+val kotlin_version = "2.0.20"
+val ktor_version = "3.0.0"
+
 plugins {
+    application
     kotlin("jvm") version "2.0.20"
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 group = "software.shonk"
@@ -15,4 +23,19 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
--- backend/api/src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-package software.shonk
-
-fun main() {
-    println("Hello World!")
-}
