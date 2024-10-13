0883987741294786c7b0e195ef3e1b0ea31344f5
nikkitschierske <nikki@schnelle.dev>
2024-10-13 20:35:41 +0200

    frontend: add prettier

--- frontend/app/eslint.config.js
+++ frontend/app/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
