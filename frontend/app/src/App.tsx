78c4d06767ca32b2ecca6cc9e41f9cc696e9ccd0
straczi <k.arendt0302@gmail.com>
2024-10-15 10:02:51 +0200

    add shadcn ThemeProvider

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 function App() {
 	return (
-		<>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<Header />
 			<div>hello world</div>
-		</>
+		</ThemeProvider>
 	);
 }
 
