24ef71ec833377fd9d7ee3126fd2b78560bf9a3e
Say <sailsman@tutanota.com>
2024-10-15 18:19:26 +0200

    [#26] set up unit testing

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -7,14 +7,14 @@ import Button from "@/components/ChangeUser.tsx";
 function App() {
 	return (
 		<>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 				<UserProvider>
 					<Header />
 					<div>hello world</div>
 					<Button></Button>
 				</UserProvider>
-            </ThemeProvider>
-        </>
+			</ThemeProvider>
+		</>
 	);
 }
 
--- frontend/app/src/exampleFile.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, it } from "@jest/globals";
-import exampleFunction from "./exampleFile.ts";
-
-it("adds a to b", () => {
-	expect(2).toBe(exampleFunction(1, 1));
-});
