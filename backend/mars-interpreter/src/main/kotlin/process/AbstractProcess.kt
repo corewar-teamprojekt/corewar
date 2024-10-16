954e4368bb072885c48975d04a85dd13d15c1c57
flamion <contact@flamion.dev>
2024-10-16 21:50:10 +0200

    [#17] Add TODO

--- backend/mars-interpreter/src/main/kotlin/process/AbstractProcess.kt
+++ backend/mars-interpreter/src/main/kotlin/process/AbstractProcess.kt
@@ -2,6 +2,7 @@ package software.shonk.interpreter.process
 
 import software.shonk.interpreter.program.AbstractProgram
 
+// TODO: Needs an equals implementation. Maybe add an id field
 abstract class AbstractProcess(val program: AbstractProgram) {
     var programCounter: Int = 0
 }
