a207110aacec1a2a700028430a6143066d0fa0fe
flamion <contact@flamion.dev>
2024-10-16 22:03:01 +0200

    [#17] Update comment

--- backend/mars-interpreter/src/main/kotlin/program/AbstractProgram.kt
+++ backend/mars-interpreter/src/main/kotlin/program/AbstractProgram.kt
@@ -9,7 +9,9 @@ import software.shonk.interpreter.IShork
 // TODO: Add List of processes
 // TODO: Accept source code for the starting / main process
 abstract class AbstractProgram(val id: String, val shork: IShork) {
-    // This function will be called once on every turn and should execute a single instruction
-    // from one of the processes contained in the program
+    /**
+     * This function will be called once on every turn and should execute a single instruction from
+     * one of the processes managed by the program
+     */
     abstract fun tick()
 }
