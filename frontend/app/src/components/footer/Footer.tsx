bc5c78387ec57785f978acae7abd80f009381b4f
Say <sailsman@tutanota.com>
2024-10-17 08:58:05 +0200

    [#24] implement simple footer

---
+++ frontend/app/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.module.css";
+
+function Footer() {
+	return (
+		<footer>
+			<div>
+				<h2 className="text-3xl font-semibold">
+					No sharks were harmed during the creation of this site.
+				</h2>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
