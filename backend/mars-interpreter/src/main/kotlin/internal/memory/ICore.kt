a1364c14f0fce122ee225ea36a98a2ce84abe978
flamion <contact@flamion.dev>
2024-10-17 23:24:22 +0200

    [#17] Add interpreter skeleton

---
+++ backend/mars-interpreter/src/main/kotlin/internal/memory/ICore.kt
@@ -0,0 +1,12 @@
+package software.shonk.interpreter.internal.memory
+
+import software.shonk.interpreter.internal.instruction.AbstractInstruction
+
+/** The interface for the memory core */
+internal interface ICore {
+    /** Loads the instruction at the given address */
+    fun loadAbsolute(address: Int): AbstractInstruction
+
+    /** Writes an instruction to the given address */
+    fun storeAbsolute(address: Int, instruction: AbstractInstruction)
+}
