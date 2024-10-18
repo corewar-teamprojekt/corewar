e97b3dac8f9f8f344895fc0c205c8d47abd3b6f8
straczi <k.arendt0302@gmail.com>
2024-10-18 11:02:52 +0200

    #51 ignore warnings for ThemeProvider

--- frontend/app/src/components/ui/ThemeProvider.tsx
+++ frontend/app/src/components/ui/ThemeProvider.tsx
@@ -1,24 +1,12 @@
-import { createContext, useContext, useEffect, useState } from "react";
-
-type Theme = "dark" | "light" | "system";
+import { Theme } from "@/enums/Theme";
+import { useEffect, useState } from "react";
+import { ThemeProviderContext } from "../hooks/useTheme";
 
 type ThemeProviderProps = {
 	children: React.ReactNode;
 	defaultTheme?: Theme;
 	storageKey?: string;
 };
-
-type ThemeProviderState = {
-	theme: Theme;
-	setTheme: (theme: Theme) => void;
-};
-
-const initialState: ThemeProviderState = {
-	theme: "system",
-	setTheme: () => null,
-};
-
-const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
 	children,
@@ -62,12 +50,3 @@ export function ThemeProvider({
 		</ThemeProviderContext.Provider>
 	);
 }
-
-export const useTheme = () => {
-	const context = useContext(ThemeProviderContext);
-
-	if (context === undefined)
-		throw new Error("useTheme must be used within a ThemeProvider");
-
-	return context;
-};
