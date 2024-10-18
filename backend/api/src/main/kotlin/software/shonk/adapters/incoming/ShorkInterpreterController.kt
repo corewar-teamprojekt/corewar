5fdf2cf00fd1fc3397201df41283abe49d8f6b7e
nikkitschierske <nikki@schnelle.dev>
2024-10-18 11:40:44 +0200

    [#20] move /code/{player} to incoming adapter

--- backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
+++ backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
@@ -1,13 +1,27 @@
 package software.shonk.adapters.incoming
 
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import software.shonk.application.port.ShorkInterpreterUseCase
 
 // Configure all endpoints regarding Hello World in here (example)
 fun Route.configureShorkInterpreterController(shorkInterpreterUseCase: ShorkInterpreterUseCase) {
     get("/status") { call.respondText(shorkInterpreterUseCase.getStatus().toString()) }
+    post("/code/{player}") {
+        val player = call.parameters["player"]
+        if (player == null || player == "") {
+            call.respond(HttpStatusCode.BadRequest)
+        } else {
+            val program = call.receive<String>()
+            shorkInterpreterUseCase.addProgram(player, program)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
 }
 
 // Localized DTOs down here
