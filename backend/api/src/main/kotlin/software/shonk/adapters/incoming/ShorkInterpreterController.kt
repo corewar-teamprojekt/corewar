6710a4a100d5df610b4f702ffc6c4f60510688eb
Maxim Schlaht <maximschlaht@gmail.com>
2024-10-21 22:43:43 +0200

    [#18, #20] add comments to endpoints

--- backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
+++ backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
@@ -9,20 +9,27 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import software.shonk.application.port.incoming.ShorkInterpreterUseCase
 
-// Configure all endpoints regarding Hello World in here (example)
+// Function to configure routes for the ShorkInterpreterController
 fun Route.configureShorkInterpreterController(shorkInterpreterUseCase: ShorkInterpreterUseCase) {
-    get("/status") { call.respondText(shorkInterpreterUseCase.getStatus().toString()) }
+    // GET endpoint to retrieve the status of the ShorkInterpreter
+    get("/status") {
+        // Respond with the current status converted to a string
+        call.respondText(shorkInterpreterUseCase.getStatus().toString())
+    }
+
+    // POST endpoint to submit code for a specific player
     post("/code/{player}") {
+        // Extract the player identifier from the URL parameters
         val player = call.parameters["player"]
+        // Respond with a Bad Request status if the player identifier is invalid
         if (player == null || player == "") {
             call.respond(HttpStatusCode.BadRequest)
         } else {
+            // Add the received program to the Shork Interpreter for the specified player
             val program = call.receive<String>()
             shorkInterpreterUseCase.addProgram(player, program)
+            // Respond with an OK status indicating successful processing
             call.respond(HttpStatusCode.OK)
         }
     }
 }
-
-// Localized DTOs down here
-// @Serializable, data call lass HelloWorldDTO...
