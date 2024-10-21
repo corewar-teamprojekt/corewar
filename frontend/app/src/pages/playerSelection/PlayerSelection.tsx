0e130a868768afaea9d56e6261efcfacd72fc488
straczi <k.arendt0302@gmail.com>
2024-10-21 16:47:21 +0200

    [#87] implement PlayerSelection Page and write tests for it

--- frontend/app/src/pages/playerSelection/PlayerSelection.tsx
+++ frontend/app/src/pages/playerSelection/PlayerSelection.tsx
@@ -1,25 +1,44 @@
 import { Button } from "@/components/ui/button.tsx";
-import { useNavigate } from "react-router-dom";
 import { useDispatchUser } from "@/services/userContext/UserContextHelpers.ts";
+import { useNavigate } from "react-router-dom";
 
 function PlayerSelection() {
 	const navigate = useNavigate();
 	const dispatcher = useDispatchUser();
 
+	function setPlayerAndRedirectToCoding(
+		actionType: "setPlayerA" | "setPlayerB",
+	) {
+		if (dispatcher) {
+			dispatcher({
+				type: actionType,
+				user: null,
+			});
+		}
+		navigate("/player-coding");
+	}
+
 	return (
-		<Button
-			onClick={() => {
-				if (dispatcher) {
-					dispatcher({
-						type: "setPlayerA",
-						user: null,
-					});
-				}
-				navigate("/player-coding");
-			}}
-		>
-			Route
-		</Button>
+		<div className="h-[100%] w-[100%] flex flex-row  justify-center items-center gap-[12%] ">
+			<div className="h-[30%] flex flex-col gap-y-5 justify-start items-center">
+				<h1 className="text-red-500 text-8xl font-extrabold">PLAYER A</h1>
+				<Button
+					className="max-w-[30%] min-w-[20%]"
+					onClick={() => setPlayerAndRedirectToCoding("setPlayerA")}
+				>
+					PLAY
+				</Button>
+			</div>
+			<div className="h-[30%] flex flex-col gap-y-5 justify-start items-center">
+				<h1 className="text-blue-500 text-8xl font-extrabold">PLAYER B</h1>
+				<Button
+					className="max-w-[30%] min-w-[20%]"
+					onClick={() => setPlayerAndRedirectToCoding("setPlayerB")}
+				>
+					PLAY
+				</Button>
+			</div>
+		</div>
 	);
 }
 
