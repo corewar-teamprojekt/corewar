3211782adbcce0a8ee838428e8fbf392037ca3a2
flamion <contact@flamion.dev>
2024-10-22 10:18:56 +0200

    [#0] Make Shork return a random winner (for now)

--- backend/shork/src/main/kotlin/Shork.kt
+++ backend/shork/src/main/kotlin/Shork.kt
@@ -2,6 +2,6 @@ package software.shonk.interpreter
 
 class Shork : IShork {
     override fun run(settings: Settings, programs: Map<String, String>): String {
-        return programs.keys.first()
+        return programs.keys.random()
     }
 }
