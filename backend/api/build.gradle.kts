7e3547edb238e677626be63283b909cdc253b890
Maxim Schlaht <maximschlaht@gmail.com>
2024-10-23 22:46:07 +0200

    [#18, #20] add content negotiation plugins and test GET and POST

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -1,47 +1,37 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val kotlin_version = "2.0.20"
-val ktor_version = "3.0.0"
+val kotlin_version: String by project
+val ktor_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "2.0.20"
     id("com.ncorti.ktfmt.gradle") version "0.20.1"
+    id("io.ktor.plugin") version "2.3.12"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     jacoco
 }
 
-ktfmt {
-    kotlinLangStyle()
-}
+ktfmt { kotlinLangStyle() }
 
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
-}
+application { mainClass.set("io.ktor.server.netty.EngineMain") }
 
 group = "software.shonk"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+dependencies { testImplementation(kotlin("test")) }
 
-dependencies {
-    implementation(project(":shork"))
-}
+dependencies { implementation(project(":shork")) }
 
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
 }
 
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-}
-
+tasks.jacocoTestReport { dependsOn(tasks.test) }
 
 repositories {
     mavenCentral()
@@ -51,7 +41,9 @@ repositories {
 dependencies {
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
@@ -59,9 +51,7 @@ dependencies {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-    }
+    compilerOptions { jvmTarget.set(JvmTarget.JVM_21) }
 }
 
 tasks.withType<JavaCompile> {
