013b78733343af77082b58e1e49b4ae6f5acd9b0
Maxim Schlaht <maximschlaht@gmail.com>
2024-10-23 22:46:07 +0200

    [#18, #20] add content negotiation plugins and test GET and POST

--- backend/api/src/main/kotlin/software/shonk/Application.kt
+++ backend/api/src/main/kotlin/software/shonk/Application.kt
@@ -1,10 +1,24 @@
 package software.shonk
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     // Install basic middleware like CORS and content negotiation here
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                classDiscriminator = "jsonType"
+                prettyPrint = true
+                useArrayPolymorphism = false
+            }
+        )
+    }
+
     configureApi()
 }
--- backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
+++ backend/api/src/main/kotlin/software/shonk/adapters/incoming/ShorkInterpreterController.kt
@@ -3,7 +3,6 @@ package software.shonk.adapters.incoming
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
@@ -14,22 +13,30 @@ fun Route.configureShorkInterpreterController(shorkInterpreterUseCase: ShorkInte
     // GET endpoint to retrieve the status of the ShorkInterpreter
     get("/status") {
         // Respond with the current status converted to a string
-        call.respondText(shorkInterpreterUseCase.getStatus().toString())
+        call.respond(shorkInterpreterUseCase.getStatus())
     }
 
     // POST endpoint to submit code for a specific player
     post("/code/{player}") {
         // Extract the player identifier from the URL parameters
         val player = call.parameters["player"]
+
         // Respond with a Bad Request status if the player identifier is invalid
-        if (player == null || player == "") {
+        if (player == null) {
             call.respond(HttpStatusCode.BadRequest)
-        } else {
-            // Add the received program to the Shork Interpreter for the specified player
-            val program = call.receive<String>()
-            shorkInterpreterUseCase.addProgram(player, program)
-            // Respond with an OK status indicating successful processing
-            call.respond(HttpStatusCode.OK)
+            return@post
         }
+
+        if (player != "playerA" && player != "playerB") {
+            call.respond(HttpStatusCode.BadRequest)
+            return@post
+        }
+
+        // Add the received program to the Shork Interpreter for the specified player
+        val program = call.receive<String>()
+        shorkInterpreterUseCase.addProgram(player, program)
+        // Respond with an OK status indicating successful processing
+        call.respond(HttpStatusCode.OK)
+        return@post
     }
 }
