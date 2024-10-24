bd49f02aa2a726d6826ad3e0330e540115eeab2a
flamion <contact@flamion.dev>
2024-10-24 14:39:34 +0200

    [#117] Explorative parser work

--- backend/shork/src/main/kotlin/internal/parser/Token.kt
+++ backend/shork/src/main/kotlin/internal/parser/Token.kt
@@ -2,6 +2,6 @@ package software.shonk.interpreter.internal.parser
 
 internal class Token(val type: TokenType, val lexeme: String, val literal: Any, val line: Int) {
     override fun toString(): String {
-        return "$type $lexeme $literal"
+        return "$type $lexeme $literal line: $line"
     }
 }
