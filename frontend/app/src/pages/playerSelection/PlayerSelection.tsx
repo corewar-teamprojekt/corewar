fa37a51f1ec9a38fb3f0f5176a3205af3b85a9e3
0815Sailsman <sailsman@tutanota.com>
2024-10-24 21:21:38 +0200

    [#100] add blocker when selecting user to avoid race condition
    I manually verified, that there can be a race condition, if there is too big of a delay between setting the user and navigating

--- frontend/app/src/pages/playerSelection/PlayerSelection.tsx
+++ frontend/app/src/pages/playerSelection/PlayerSelection.tsx
@@ -2,25 +2,29 @@ import { RequireLogout } from "@/components/requireLogout/RequireLogout";
 import { Button } from "@/components/ui/button.tsx";
 import { useDispatchUser } from "@/services/userContext/UserContextHelpers.ts";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function PlayerSelection() {
 	const navigate = useNavigate();
 	const dispatcher = useDispatchUser();
+	const [blockLogout, setBlockLogout] = useState<boolean>(false);
 
 	function setPlayerAndRedirectToCoding(
 		actionType: "setPlayerA" | "setPlayerB",
 	) {
 		if (dispatcher) {
+			setBlockLogout(true);
 			dispatcher({
 				type: actionType,
 				user: null,
 			});
 		}
 		navigate("/player-coding");
+		setBlockLogout(false);
 	}
 
 	return (
-		<RequireLogout>
+		<RequireLogout blocked={blockLogout}>
 			<div className="h-[100%] w-[100%] flex flex-row  justify-center items-center gap-[12%] ">
 				<div className="h-[30%] flex flex-col gap-y-5 justify-start items-center">
 					<h1 className="text-red-500 text-8xl font-extrabold">PLAYER A</h1>
