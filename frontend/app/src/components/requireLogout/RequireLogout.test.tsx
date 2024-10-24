66a08b00fe466ef9988d2fbdc8c9aea686dc60d8
straczi <k.arendt0302@gmail.com>
2024-10-24 13:20:37 +0200

    [#100] implement RequireLogout and write tests

---
+++ frontend/app/src/components/requireLogout/RequireLogout.test.tsx
@@ -0,0 +1,44 @@
+import {
+	useDispatchUser,
+	useUser,
+} from "@/services/userContext/UserContextHelpers";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, Mock, vi } from "vitest";
+import { RequireLogout } from "./RequireLogout";
+
+vi.mock("@/services/userContext/UserContextHelpers");
+
+describe("RequireLogout", () => {
+	const requiredText = "No users here pwease :3";
+
+	it("renders children when user is not logged in", () => {
+		(useUser as Mock).mockReturnValue(null);
+		(useDispatchUser as Mock).mockReturnValue(vi.fn());
+
+		render(
+			<RequireLogout>
+				<p>{requiredText}</p>
+			</RequireLogout>,
+		);
+		screen.debug();
+		expect(screen.getByText(requiredText)).toBeInTheDocument();
+	});
+
+	it("dispatches logout action when user is logged in", () => {
+		const mockDispatcher = vi.fn();
+		(useUser as Mock).mockReturnValue({ name: "testUser" });
+		(useDispatchUser as Mock).mockReturnValue(mockDispatcher);
+
+		render(
+			<RequireLogout>
+				<p>{requiredText}</p>
+			</RequireLogout>,
+		);
+
+		expect(mockDispatcher).toHaveBeenCalledWith({
+			type: "logout",
+			user: null,
+		});
+	});
+});
---
+++ frontend/app/src/components/requireLogout/RequireLogout.tsx
@@ -0,0 +1,27 @@
+import {
+	useDispatchUser,
+	useUser,
+} from "@/services/userContext/UserContextHelpers";
+import { ReactNode, useEffect } from "react";
+
+interface RequireUserProps {
+	children: ReactNode;
+}
+
+export function RequireLogout({ children }: Readonly<RequireUserProps>) {
+	const user = useUser();
+	const dispatcher = useDispatchUser();
+
+	useEffect(handleUserChanges, [user]);
+
+	function handleUserChanges() {
+		if (user && dispatcher) {
+			dispatcher({
+				type: "logout",
+				user: null,
+			});
+		}
+	}
+
+	return <>{children}</>;
+}
