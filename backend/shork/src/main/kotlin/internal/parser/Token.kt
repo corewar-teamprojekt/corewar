fc75462338e6686900732d958eb7bc26ad85eb59
flamion <contact@flamion.dev>
2024-10-25 21:08:44 +0200

    [#117] Add single line tests for Tokenizer (Scanner)

--- backend/shork/src/main/kotlin/internal/parser/Token.kt
+++ backend/shork/src/main/kotlin/internal/parser/Token.kt
@@ -4,4 +4,26 @@ internal class Token(val type: TokenType, val lexeme: String, val literal: Any, 
     override fun toString(): String {
         return "$type $lexeme $literal line: $line"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Token
+
+        if (type != other.type) return false
+        if (lexeme != other.lexeme) return false
+        if (literal != other.literal) return false
+        if (line != other.line) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = type.hashCode()
+        result = 31 * result + lexeme.hashCode()
+        result = 31 * result + literal.hashCode()
+        result = 31 * result + line
+        return result
+    }
 }
