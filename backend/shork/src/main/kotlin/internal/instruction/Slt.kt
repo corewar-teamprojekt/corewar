44fd6e043af64ce29fe5f19d6165a5d6fa85bc76
flamion <contact@flamion.dev>
2024-10-28 11:12:22 +0100

    [#130] Add stubs for not yet implemented instructions

---
+++ backend/shork/src/main/kotlin/internal/instruction/Slt.kt
@@ -0,0 +1,21 @@
+package software.shonk.interpreter.internal.instruction
+
+import software.shonk.interpreter.internal.addressing.AddressMode
+import software.shonk.interpreter.internal.addressing.Modifier
+import software.shonk.interpreter.internal.process.AbstractProcess
+
+internal class Slt(
+    aField: Int,
+    bField: Int,
+    addressModeA: AddressMode,
+    addressModeB: AddressMode,
+    modifier: Modifier,
+) : AbstractInstruction(aField, bField, addressModeA, addressModeB, modifier) {
+    override fun execute(process: AbstractProcess) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deepCopy(): AbstractInstruction {
+        TODO("Not yet implemented")
+    }
+}
