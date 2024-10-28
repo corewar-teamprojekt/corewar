c13064ee4ba5ab52943b3bb9b5476e2f16187b0b
Piet Helzel <piet.helzel@outlook.com>
2024-10-28 21:16:06 +0100

    [#59, #60, #61, #62] Implement SUB, MUL, DIV, MOD instructions

--- backend/shork/src/main/kotlin/internal/instruction/Sub.kt
+++ backend/shork/src/main/kotlin/internal/instruction/Sub.kt
@@ -2,7 +2,6 @@ package software.shonk.interpreter.internal.instruction
 
 import software.shonk.interpreter.internal.addressing.AddressMode
 import software.shonk.interpreter.internal.addressing.Modifier
-import software.shonk.interpreter.internal.process.AbstractProcess
 
 internal class Sub(
     aField: Int,
@@ -10,12 +9,13 @@ internal class Sub(
     addressModeA: AddressMode,
     addressModeB: AddressMode,
     modifier: Modifier,
-) : AbstractInstruction(aField, bField, addressModeA, addressModeB, modifier) {
-    override fun execute(process: AbstractProcess) {
-        TODO("Not yet implemented")
+) : AbstractArithmeticInstruction(aField, bField, addressModeA, addressModeB, modifier) {
+
+    override fun runArithmeticOperation(operand1: Int, operand2: Int): Int {
+        return operand1 - operand2
     }
 
     override fun deepCopy(): AbstractInstruction {
-        TODO("Not yet implemented")
+        return Sub(aField, bField, addressModeA, addressModeB, modifier)
     }
 }
