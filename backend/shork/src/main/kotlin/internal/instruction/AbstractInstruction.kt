d3f10b3707baec836ec4206286da0c7c8c4df477
flamion <contact@flamion.dev>
2024-10-28 11:28:16 +0100

    [#135] Implement default equals implementation for instructions via the AbstractInstruction

--- backend/shork/src/main/kotlin/internal/instruction/AbstractInstruction.kt
+++ backend/shork/src/main/kotlin/internal/instruction/AbstractInstruction.kt
@@ -62,4 +62,28 @@ internal abstract class AbstractInstruction(
             }
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AbstractInstruction
+
+        if (aField != other.aField) return false
+        if (bField != other.bField) return false
+        if (addressModeA != other.addressModeA) return false
+        if (addressModeB != other.addressModeB) return false
+        if (modifier != other.modifier) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = aField
+        result = 31 * result + bField
+        result = 31 * result + addressModeA.hashCode()
+        result = 31 * result + addressModeB.hashCode()
+        result = 31 * result + modifier.hashCode()
+        return result
+    }
 }
