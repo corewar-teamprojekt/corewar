522d8a2fca60d626e523693b978442edf3e41270
Piet Helzel <piet.helzel@outlook.com>
2024-10-28 21:15:13 +0100

    [#58, #59, #60, #61, #62] Implement basic tests for all arithmetic instructions

--- backend/shork/src/test/kotlin/instruction/TestDiv.kt
+++ backend/shork/src/test/kotlin/instruction/TestDiv.kt
@@ -6,150 +6,150 @@ import org.junit.jupiter.api.Test
 import software.shonk.interpreter.InternalShork
 import software.shonk.interpreter.internal.addressing.AddressMode
 import software.shonk.interpreter.internal.addressing.Modifier
-import software.shonk.interpreter.internal.instruction.Add
 import software.shonk.interpreter.internal.instruction.Dat
+import software.shonk.interpreter.internal.instruction.Div
 import software.shonk.interpreter.internal.process.Process
 import software.shonk.interpreter.internal.program.Program
 import software.shonk.interpreter.internal.settings.InternalSettings
 
-internal class TestAdd {
+internal class TestDiv {
 
-    private val dat = Dat(5, 7, AddressMode.IMMEDIATE, AddressMode.IMMEDIATE, Modifier.A)
+    private val dat = Dat(5, 13, AddressMode.IMMEDIATE, AddressMode.IMMEDIATE, Modifier.A)
     private val settings = InternalSettings(8000, 1000, dat, 1000)
     private var shork = InternalShork(settings)
-    private var program = Program("add", shork)
+    private var program = Program("div", shork)
     private var process = Process(program, 0)
 
     @BeforeEach
     fun setup() {
         shork = InternalShork(settings)
-        program = Program("add", shork)
+        program = Program("div", shork)
         process = Process(program, 0)
     }
 
     @Test
     fun testExecuteA() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.A)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.A)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
-        assert(resultInstruction.aField == 10)
-        assert(resultInstruction.bField == 7)
+        assert(resultInstruction.aField == 1)
+        assert(resultInstruction.bField == 13)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteB() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.B)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.B)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
         assert(resultInstruction.aField == 5)
-        assert(resultInstruction.bField == 14)
+        assert(resultInstruction.bField == 1)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteAB() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.AB)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.AB)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
         assert(resultInstruction.aField == 5)
-        assert(resultInstruction.bField == 12)
+        assert(resultInstruction.bField == 0)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteBA() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.BA)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.BA)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
-        assert(resultInstruction.aField == 12)
-        assert(resultInstruction.bField == 7)
+        assert(resultInstruction.aField == 2)
+        assert(resultInstruction.bField == 13)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteF() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.F)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.F)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
-        assert(resultInstruction.aField == 10)
-        assert(resultInstruction.bField == 14)
+        assert(resultInstruction.aField == 1)
+        assert(resultInstruction.bField == 1)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteI() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.I)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.I)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
-        assert(resultInstruction.aField == 10)
-        assert(resultInstruction.bField == 14)
+        assert(resultInstruction.aField == 1)
+        assert(resultInstruction.bField == 1)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
     fun testExecuteX() {
-        val add = Add(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.X)
-        shork.memoryCore.storeAbsolute(0, add)
+        val div = Div(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, Modifier.X)
+        shork.memoryCore.storeAbsolute(0, div)
 
-        add.execute(process)
+        div.execute(process)
 
         var resultInstruction = shork.memoryCore.loadAbsolute(2)
 
         assert(resultInstruction is Dat)
-        assert(resultInstruction.aField == 12)
-        assert(resultInstruction.bField == 12)
+        assert(resultInstruction.aField == 2)
+        assert(resultInstruction.bField == 0)
         assert(resultInstruction.addressModeA == AddressMode.IMMEDIATE)
         assert(resultInstruction.addressModeB == AddressMode.IMMEDIATE)
     }
 
     @Test
-    fun testDeepCopy() {
-        val add = Add(42, 69, AddressMode.DIRECT, AddressMode.IMMEDIATE, Modifier.A)
-        val copy = add.deepCopy()
+    fun div() {
+        val div = Div(42, 69, AddressMode.DIRECT, AddressMode.IMMEDIATE, Modifier.A)
+        val copy = div.deepCopy()
 
-        assert(copy is Add)
-        assertEquals(add.aField, copy.aField)
-        assertEquals(add.bField, copy.bField)
-        assertEquals(add.addressModeA, copy.addressModeA)
-        assertEquals(add.addressModeB, copy.addressModeB)
-        assertEquals(add.modifier, copy.modifier)
-        assert(add !== copy)
+        assert(copy is Div)
+        assertEquals(div.aField, copy.aField)
+        assertEquals(div.bField, copy.bField)
+        assertEquals(div.addressModeA, copy.addressModeA)
+        assertEquals(div.addressModeB, copy.addressModeB)
+        assertEquals(div.modifier, copy.modifier)
+        assert(div !== copy)
     }
 }
