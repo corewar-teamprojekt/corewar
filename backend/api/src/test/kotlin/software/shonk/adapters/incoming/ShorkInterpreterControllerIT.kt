9641b8dde4de0783f240cacd984ac1879afad889
nikkitschierske <nikki@schnelle.dev>
2024-11-03 16:39:23 +0100

    [#0] api tests: add own runTest block

--- backend/api/src/test/kotlin/software/shonk/adapters/incoming/ShorkInterpreterControllerIT.kt
+++ backend/api/src/test/kotlin/software/shonk/adapters/incoming/ShorkInterpreterControllerIT.kt
@@ -73,6 +73,10 @@ class ShorkInterpreterControllerIT() : KoinTest {
         )
     }
 
+    private fun runTest(testBlock: suspend TestApplicationEngine.() -> Unit) {
+        with(testEngine) { runBlocking { testBlock() } }
+    }
+
     @Test
     fun testGetStatus() =
         with(testEngine) {
@@ -83,170 +87,134 @@ class ShorkInterpreterControllerIT() : KoinTest {
         }
 
     @Test
-    fun testGetStatusDefault() =
-        with(testEngine) {
-            runBlocking {
-                val response = client.get("/api/v0/status")
-                val responseData = parseStatus(response)
-                assertEquals("false", responseData["playerASubmitted"])
-                assertEquals("false", responseData["playerBSubmitted"])
-                assertEquals("NOT_STARTED", responseData["gameState"])
-                assertEquals("UNDECIDED", responseData["result.winner"])
-            }
-        }
-
-    @Test
-    fun testPostPlayerCodeValidUsername() =
-        with(testEngine) {
-            runBlocking {
-                val player = "playerA"
-                val response = client.post("/api/v0/code/$player")
-                assertEquals(HttpStatusCode.OK, response.status)
-            }
-        }
-
-    @Test
-    fun testPostPlayerCodeInvalidUsername() =
-        with(testEngine) {
-            runBlocking {
-                val player = "playerC"
-                val response = client.post("/api/v0/code/$player")
-                assertEquals(HttpStatusCode.BadRequest, response.status)
-            }
-        }
-
-    @Test
-    fun testPostPlayerCodeValid() =
-        with(testEngine) {
-            runBlocking {
-                val player = "playerA"
-                val response =
-                    client.post("/api/v0/code/$player") {
-                        contentType(ContentType.Application.Json)
-                        setBody("somestring")
-                    }
-                assertEquals(HttpStatusCode.OK, response.status)
-            }
-        }
-
-    @Test
-    fun testPlayerSubmitted() =
-        with(testEngine) {
-            runBlocking {
-                val player = "playerA"
-                client.post("/api/v0/code/$player") {
-                    contentType(ContentType.Application.Json)
-                    setBody("somestring")
-                }
-
-                val response = client.get("/api/v0/status")
-                val responseData = parseStatus(response)
-                assertEquals("true", responseData["playerASubmitted"])
-                assertEquals("false", responseData["playerBSubmitted"])
-                assertEquals("NOT_STARTED", responseData["gameState"])
-                assertEquals("UNDECIDED", responseData["result.winner"])
-            }
-        }
-
-    @Test
-    fun testInvalidPlayerSubmitted() =
-        with(testEngine) {
-            runBlocking {
-                val player = "playerC"
-                client.post("/api/v0/code/$player") {
-                    contentType(ContentType.Application.Json)
-                    setBody("somestring")
-                }
-
-                val response = client.get("/api/v0/status")
-                val responseData = parseStatus(response)
-                assertEquals("false", responseData["playerASubmitted"])
-                assertEquals("false", responseData["playerBSubmitted"])
-                assertEquals("NOT_STARTED", responseData["gameState"])
-                assertEquals("UNDECIDED", responseData["result.winner"])
-            }
-        }
-
-    @Test
-    fun testBothPlayersSubmittedAndGameStartsAndLongerProgramWins() {
-        with(testEngine) {
-            runBlocking {
-                client.post("/api/v0/code/playerA") {
-                    contentType(ContentType.Application.Json)
-                    setBody("looooooooong")
-                }
-
-                client.post("/api/v0/code/playerB") {
-                    contentType(ContentType.Application.Json)
-                    setBody("short")
-                }
-
-                val response = client.get("/api/v0/status")
-                val responseData = parseStatus(response)
-
-                assertNotEquals("NOT_STARTED", responseData["gameState"])
-                assertEquals("A", responseData["result.winner"])
-            }
-        }
+    fun testGetStatusDefault() = runTest {
+        val response = client.get("/api/v0/status")
+        val responseData = parseStatus(response)
+        assertEquals("false", responseData["playerASubmitted"])
+        assertEquals("false", responseData["playerBSubmitted"])
+        assertEquals("NOT_STARTED", responseData["gameState"])
+        assertEquals("UNDECIDED", responseData["result.winner"])
     }
 
     @Test
-    fun testBothPlayersSubmittedGameGetsSimulatedPlayerCodeAndFlagsResetAfterNewPlayerCodeGetsSubmitted() {
-        with(testEngine) {
-            runBlocking {
-                client.post("/api/v0/code/playerA") {
-                    contentType(ContentType.Application.Json)
-                    setBody("someString")
-                }
-
-                client.post("/api/v0/code/playerB") {
-                    contentType(ContentType.Application.Json)
-                    setBody("someOtherString")
-                }
-                client.post("/api/v0/code/playerA") {
-                    contentType(ContentType.Application.Json)
-                    setBody("someNewString")
-                }
-
-                // check if code/flags were reset after running and game hasn't started
-                val response = client.get("/api/v0/status")
-                val resopnseData = parseStatus(response)
-                assertEquals("true", resopnseData["playerASubmitted"])
-                assertEquals("false", resopnseData["playerBSubmitted"])
-                assertEquals("NOT_STARTED", resopnseData["gameState"])
-                assertEquals("UNDECIDED", resopnseData["result.winner"])
-            }
-        }
+    fun testPostPlayerCodeValidUsername() = runTest {
+        val player = "playerA"
+        val response = client.post("/api/v0/code/$player")
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 
     @Test
-    fun testGetPlayerACode() {
-        with(testEngine) {
-            runBlocking {
-                client.post("/api/v0/code/playerA") {
-                    contentType(ContentType.Application.Json)
-                    setBody("someString")
-                }
-                val result = client.get("/api/v0/code/playerA")
-                assertEquals(
-                    "someString",
-                    Json.parseToJsonElement(result.bodyAsText())
-                        .jsonObject["code"]
-                        ?.jsonPrimitive
-                        ?.content,
-                )
-            }
-        }
+    fun testPostPlayerCodeInvalidUsername() = runTest {
+        val player = "playerC"
+        val response = client.post("/api/v0/code/$player")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
     }
 
     @Test
-    fun testGetPlayerCodeNotSubmitted() {
-        with(testEngine) {
-            runBlocking {
-                val result = client.get("/api/v0/code/playerA")
-                assertEquals(HttpStatusCode.BadRequest, result.status)
-                assert(result.bodyAsText().contains("No player with that name in the lobby"))
+    fun testPostPlayerCodeValid() = runTest {
+        val player = "playerA"
+        val response =
+            client.post("/api/v0/code/$player") {
+                contentType(ContentType.Application.Json)
+                setBody("somestring")
             }
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun testPlayerSubmitted() = runTest {
+        val player = "playerA"
+        client.post("/api/v0/code/$player") {
+            contentType(ContentType.Application.Json)
+            setBody("somestring")
         }
+
+        val response = client.get("/api/v0/status")
+        val responseData = parseStatus(response)
+        assertEquals("true", responseData["playerASubmitted"])
+        assertEquals("false", responseData["playerBSubmitted"])
+        assertEquals("NOT_STARTED", responseData["gameState"])
+        assertEquals("UNDECIDED", responseData["result.winner"])
+    }
+
+    @Test
+    fun testInvalidPlayerSubmitted() = runTest {
+        val player = "playerC"
+        client.post("/api/v0/code/$player") {
+            contentType(ContentType.Application.Json)
+            setBody("somestring")
+        }
+
+        val response = client.get("/api/v0/status")
+        val responseData = parseStatus(response)
+        assertEquals("false", responseData["playerASubmitted"])
+        assertEquals("false", responseData["playerBSubmitted"])
+        assertEquals("NOT_STARTED", responseData["gameState"])
+        assertEquals("UNDECIDED", responseData["result.winner"])
+    }
+
+    @Test
+    fun testBothPlayersSubmittedAndGameStartsAndLongerProgramWins() = runTest {
+        client.post("/api/v0/code/playerA") {
+            contentType(ContentType.Application.Json)
+            setBody("looooooooong")
+        }
+
+        client.post("/api/v0/code/playerB") {
+            contentType(ContentType.Application.Json)
+            setBody("short")
+        }
+
+        val response = client.get("/api/v0/status")
+        val responseData = parseStatus(response)
+
+        assertNotEquals("NOT_STARTED", responseData["gameState"])
+        assertEquals("A", responseData["result.winner"])
+    }
+
+    @Test
+    fun testBothPlayersSubmittedGameGetsSimulatedPlayerCodeAndFlagsResetAfterNewPlayerCodeGetsSubmitted() =
+        runTest {
+            client.post("/api/v0/code/playerA") {
+                contentType(ContentType.Application.Json)
+                setBody("someString")
+            }
+
+            client.post("/api/v0/code/playerB") {
+                contentType(ContentType.Application.Json)
+                setBody("someOtherString")
+            }
+            client.post("/api/v0/code/playerA") {
+                contentType(ContentType.Application.Json)
+                setBody("someNewString")
+            }
+
+            // check if code/flags were reset after running and game hasn't started
+            val response = client.get("/api/v0/status")
+            val resopnseData = parseStatus(response)
+            assertEquals("true", resopnseData["playerASubmitted"])
+            assertEquals("false", resopnseData["playerBSubmitted"])
+            assertEquals("NOT_STARTED", resopnseData["gameState"])
+            assertEquals("UNDECIDED", resopnseData["result.winner"])
+        }
+
+    @Test
+    fun testGetPlayerACode() = runTest {
+        client.post("/api/v0/code/playerA") {
+            contentType(ContentType.Application.Json)
+            setBody("someString")
+        }
+        val result = client.get("/api/v0/code/playerA")
+        assertEquals(
+            "someString",
+            Json.parseToJsonElement(result.bodyAsText()).jsonObject["code"]?.jsonPrimitive?.content,
+        )
+    }
+
+    @Test
+    fun testGetPlayerCodeNotSubmitted() = runTest {
+        val result = client.get("/api/v0/code/playerA")
+        assertEquals(HttpStatusCode.BadRequest, result.status)
+        assert(result.bodyAsText().contains("No player with that name in the lobby"))
     }
 }
