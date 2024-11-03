6ba7a774f9adb0ea420614b401cb43294209f835
nikkitschierske <nikki@schnelle.dev>
2024-11-03 22:29:00 +0100

    [#133] implement SEQ instruction

---
+++ backend/shork/src/test/kotlin/instruction/TestSeq.kt
@@ -0,0 +1,177 @@
+package instruction
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import software.shonk.interpreter.InternalShork
+import software.shonk.interpreter.internal.addressing.AddressMode
+import software.shonk.interpreter.internal.addressing.Modifier
+import software.shonk.interpreter.internal.instruction.Dat
+import software.shonk.interpreter.internal.instruction.Seq
+import software.shonk.interpreter.internal.process.Process
+import software.shonk.interpreter.internal.program.Program
+import software.shonk.interpreter.internal.settings.InternalSettings
+
+internal class TestSeq {
+    private val dat = Dat(0, 0, AddressMode.IMMEDIATE, AddressMode.IMMEDIATE, Modifier.A)
+    private val settings = InternalSettings(8000, 1000, dat, 1000)
+    private var shork = InternalShork(settings)
+    private var program = Program("seq", shork)
+    private var process = Process(program, 0)
+
+    @BeforeEach
+    fun setup() {
+        shork = InternalShork(settings)
+        program = Program("seq", shork)
+        process = Process(program, 0)
+    }
+
+    private fun executeSeqAndAssertCounter(seq: Seq, expectedCounterIncrement: Int) {
+        seq.execute(process)
+        assertEquals(expectedCounterIncrement, process.programCounter)
+    }
+
+    private fun setupSeq(modifier: Modifier): Seq {
+        val seq = Seq(1, 2, AddressMode.DIRECT, AddressMode.DIRECT, modifier)
+        shork.memoryCore.storeAbsolute(0, seq)
+        return seq
+    }
+
+    private fun setupMemory(aField: Int, bField: Int, address: Int) {
+        val dat = Dat(aField, bField, AddressMode.IMMEDIATE, AddressMode.IMMEDIATE, Modifier.A)
+        shork.memoryCore.storeAbsolute(address, dat)
+    }
+
+    @Test
+    fun testExecuteAMatch() {
+        val seq = setupSeq(Modifier.A)
+        setupMemory(42, 0, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteANoMatch() {
+        val seq = setupSeq(Modifier.A)
+        setupMemory(40, 0, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testExecuteBMatch() {
+        val seq = setupSeq(Modifier.B)
+        setupMemory(0, 69, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteBNoMatch() {
+        val seq = setupSeq(Modifier.B)
+        setupMemory(0, 40, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testExecuteFMatch() {
+        val seq = setupSeq(Modifier.F)
+        setupMemory(42, 69, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteFNoMatch() {
+        val seq = setupSeq(Modifier.F)
+        setupMemory(42, 69, 1)
+        setupMemory(69, 42, 2)
+        executeSeqAndAssertCounter(seq, 0)
+
+        setupMemory(42, 69, 1)
+        setupMemory(42, 70, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testExecuteIMatch() {
+        val seq = setupSeq(Modifier.I)
+        setupMemory(42, 69, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteINoMatch() {
+        val seq = setupSeq(Modifier.I)
+        setupMemory(42, 69, 1)
+        setupMemory(69, 42, 2)
+        executeSeqAndAssertCounter(seq, 0) // no match, so skip next instruction
+    }
+
+    @Test
+    fun testExecuteABMatch() {
+        val seq = setupSeq(Modifier.AB)
+        setupMemory(42, 69, 1)
+        setupMemory(69, 42, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteABNoMatch() {
+        val seq = setupSeq(Modifier.AB)
+        setupMemory(42, 69, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testExecuteBAMatch() {
+        val seq = setupSeq(Modifier.BA)
+        setupMemory(69, 42, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteBANoMatch() {
+        val seq = setupSeq(Modifier.BA)
+        setupMemory(69, 42, 1)
+        setupMemory(69, 42, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testExecuteXMatch() {
+        val seq = setupSeq(Modifier.X)
+        setupMemory(42, 69, 1)
+        setupMemory(69, 42, 2)
+        executeSeqAndAssertCounter(seq, 1)
+    }
+
+    @Test
+    fun testExecuteXNoMatch() {
+        val seq = setupSeq(Modifier.X)
+        setupMemory(42, 69, 1)
+        setupMemory(42, 69, 2)
+        executeSeqAndAssertCounter(seq, 0)
+
+        setupMemory(42, 69, 1)
+        setupMemory(70, 42, 2)
+        executeSeqAndAssertCounter(seq, 0)
+    }
+
+    @Test
+    fun testDeepCopy() {
+        val seq = Seq(42, 69, AddressMode.DIRECT, AddressMode.IMMEDIATE, Modifier.A)
+        val copy = seq.deepCopy()
+
+        assertEquals(seq.aField, copy.aField)
+        assertEquals(seq.bField, copy.bField)
+        assertEquals(seq.addressModeA, copy.addressModeA)
+        assertEquals(seq.addressModeB, copy.addressModeB)
+        assertEquals(seq.modifier, copy.modifier)
+        assert(seq !== copy)
+    }
+}
