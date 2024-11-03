518116f293e27bf1dd2a45bbc9e6ec5c321b1da1
nikkitschierske <nikki@schnelle.dev>
2024-11-03 22:29:00 +0100

    [#133] implement SEQ instruction

--- backend/shork/src/main/kotlin/internal/instruction/Seq.kt
+++ backend/shork/src/main/kotlin/internal/instruction/Seq.kt
@@ -12,11 +12,46 @@ internal class Seq(
     modifier: Modifier,
 ) : AbstractInstruction(aField, bField, addressModeA, addressModeB, modifier) {
     override fun execute(process: AbstractProcess) {
-        TODO("Not yet implemented")
+        val core = process.program.shork.memoryCore
+        val sourceAddress = resolve(process, aField, addressModeA)
+        val destinationAddress = resolve(process, bField, addressModeB)
+        val sourceInstruction = core.loadAbsolute(sourceAddress)
+        val destinationInstruction = core.loadAbsolute(destinationAddress)
+
+        val match =
+            when (modifier) {
+                Modifier.A -> {
+                    sourceInstruction.aField == destinationInstruction.aField
+                }
+                Modifier.B -> {
+                    sourceInstruction.bField == destinationInstruction.bField
+                }
+                Modifier.AB -> {
+                    sourceInstruction.aField == destinationInstruction.bField
+                }
+                Modifier.BA -> {
+                    sourceInstruction.bField == destinationInstruction.aField
+                }
+                Modifier.F -> {
+                    sourceInstruction.aField == destinationInstruction.aField &&
+                        sourceInstruction.bField == destinationInstruction.bField
+                }
+                Modifier.X -> {
+                    sourceInstruction.aField == destinationInstruction.bField &&
+                        sourceInstruction.bField == destinationInstruction.aField
+                }
+                Modifier.I -> {
+                    sourceInstruction == destinationInstruction
+                }
+            }
+
+        if (match) {
+            process.programCounter++
+        }
     }
 
     override fun deepCopy(): AbstractInstruction {
-        TODO("Not yet implemented")
+        return Seq(aField, bField, addressModeA, addressModeB, modifier)
     }
 
     override fun toString(): String {
