b2aa4c7ee4c8af31455c9dfa03b6cf3cbf41d9ba
Straczi <k.arendt0302@gmail.com>
2024-11-04 13:28:54 +0100

    [#150] try out using linter warnings and errors

--- frontend/app/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/app/src/components/CodeEditor/CodeEditor.tsx
@@ -8,6 +8,15 @@ interface CodeEditorProps {
 export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 	const monaco = useMonaco();
 
+	const testProgram = `; heheheha, code here :D
+
+	start   ADD #4, bomb_target   ; Increment bomb target by 4
+        	MOV bomb, @bomb_target ; Place a bomb at the new target location
+        	JMP start             ; Loop back to start
+
+	bomb    	DAT #0                ; The bomb - this will terminate an enemy process if hit
+	bomb_target DAT #0            ; Starting location for bombing (relative to start)`;
+
 	useEffect(() => {
 		if (monaco) {
 			// Register the custom language
@@ -62,6 +71,28 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 					],
 				},
 			});
+			const model = monaco.editor.getModels()[0];
+			const markers = [
+				{
+					severity: monaco.MarkerSeverity.Warning,
+					startLineNumber: 2,
+					startColumn: 1,
+					endLineNumber: 2,
+					endColumn: 50,
+					message: "whoopsies there is an wornin in line 2 >w<",
+				},
+				{
+					severity: monaco.MarkerSeverity.Error,
+					startLineNumber: 4,
+					startColumn: 1,
+					endLineNumber: 4,
+					endColumn: 50,
+					message: "whoopsies there is an ewwor in line 4 >w<",
+				},
+			];
+
+			// Set markers on the model
+			monaco.editor.setModelMarkers(model, "customLinter", markers);
 		}
 	}, [monaco]);
 
@@ -70,7 +101,7 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 			height="30vh"
 			theme="vs-dark"
 			defaultLanguage="redcode"
-			defaultValue="// heheheha, code here :D "
+			defaultValue={testProgram}
 			onChange={(value) => setProgram(value ?? "")}
 		/>
 	);
