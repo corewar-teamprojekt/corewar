67e427ed511ca6c1c4901ddc7f52ad048908a030
flamion <contact@flamion.dev>
2024-11-05 19:02:45 +0100

    [#71] Add tests for InternalShork

--- backend/shork/src/main/kotlin/internal/settings/InternalSettings.kt
+++ backend/shork/src/main/kotlin/internal/settings/InternalSettings.kt
@@ -3,8 +3,20 @@ package software.shonk.interpreter.internal.settings
 import software.shonk.interpreter.internal.instruction.AbstractInstruction
 
 internal class InternalSettings(
+    /** The size of the core, in number of instructions it will fit. */
     val coreSize: Int,
+    /**
+     * The maximum number of instructions will be executed before the interpreter stops and a draw
+     * is called.
+     */
     val instructionLimit: Int,
+    /** The initial instruction the core will be filled with. */
     val initialInstruction: AbstractInstruction,
-    val maximumTicks: Int,
+    /**
+     * The maximum number of cycles the interpreter will run before stopping. A cycle is an
+     * execution of a single instruction of every players' program.
+     */
+    val maximumCycles: Int,
+    /** The minimum separation between two processes. */
+    val minimumSeparation: Int,
 )
