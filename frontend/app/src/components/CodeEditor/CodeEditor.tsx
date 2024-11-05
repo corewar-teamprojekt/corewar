7b116905b0ecc7d76d7ecc01c345f700feaa2274
straczi <k.arendt0302@gmail.com>
2024-11-05 12:56:40 +0100

    [#150] move redcode language setup into monacoConfig

--- frontend/app/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/app/src/components/CodeEditor/CodeEditor.tsx
@@ -17,97 +17,11 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 	bomb    	DAT #0                ; The bomb: will terminate an enemy process if hit
 	bomb_target DAT #0            ; Starting location for bombing (relative to start)`;
 
-	const keywordsList = [
-		"MOV",
-		"ADD",
-		"SUB",
-		"MUL",
-		"DIV",
-		"MOD",
-		"JMP",
-		"JMZ",
-		"JMN",
-		"DJN",
-		"CMP",
-		"SLT",
-		"SPL",
-		"DAT",
-		"NOP",
-	];
-
 	useEffect(() => {
 		if (monaco) {
-			// Register the custom language
-			monaco.languages.register({ id: "redcode" });
-			//can't tell you why yet, but this way it do worky :sunglasses:
-			setTimeout(setUpTokenProvider, 100);
-			setTimeout(setUpAutoCompletion, 200);
-			setTimeout(setUpTestingLinterWarningsAndErrors, 300);
+			setTimeout(setUpTestingLinterWarningsAndErrors, 500);
 		}
 	}, [monaco]);
-
-	function setUpTokenProvider() {
-		if (!monaco) return;
-
-		// Define the language tokens and syntax highlighting
-		monaco.languages.setMonarchTokensProvider("redcode", {
-			keywords: keywordsList,
-			operators: ["#", "$", "@", "<", ">", "{", "}"],
-			symbols: /[@#$,]/,
-
-			// Define tokenizer rules
-			tokenizer: {
-				root: [
-					// Comments
-					[/(;.*$)/, "comment"],
-
-					// Labels
-					[/^[a-zA-Z_]\w*:/, "type.identifier"],
-
-					// Instructions (keywords)
-					[
-						/\b(MOV|ADD|SUB|MUL|DIV|MOD|JMP|JMZ|JMN|DJN|CMP|SLT|SPL|DAT|NOP)\b/,
-						"keyword",
-					],
-
-					// Numbers
-					[/\b\d+\b/, "number"],
-
-					// Operators (e.g., addressing modes)
-					[/[#$@<>]/, "operator"],
-
-					// Any other symbol
-					[/[.,]/, "delimiter"],
-				],
-			},
-		});
-	}
-
-	function setUpAutoCompletion() {
-		if (!monaco) return;
-		const model = monaco.editor.getModels()[0];
-		monaco.languages.registerCompletionItemProvider("redcode", {
-			provideCompletionItems: (_, position) => {
-				// Get the word until the current position to find the appropriate range
-				const wordInfo = model.getWordUntilPosition(position);
-
-				// Define the range for the completion item
-				const range = new monaco.Range(
-					position.lineNumber,
-					wordInfo.startColumn,
-					position.lineNumber,
-					wordInfo.endColumn,
-				);
-				const suggestions = keywordsList.map((keyword) => ({
-					label: keyword,
-					kind: monaco.languages.CompletionItemKind.Keyword,
-					insertText: keyword,
-					range: range,
-				}));
-				return { suggestions };
-			},
-		});
-	}
 
 	function setUpTestingLinterWarningsAndErrors() {
 		if (!monaco) return;
