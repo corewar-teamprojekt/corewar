fc936c27740ac7006cf7300d2730ce52d68e54a7
straczi <k.arendt0302@gmail.com>
2024-11-06 08:25:07 +0100

    [#180] implement function to add linter linting

--- frontend/app/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/app/src/components/CodeEditor/CodeEditor.tsx
@@ -1,4 +1,5 @@
 import { Editor, useMonaco } from "@monaco-editor/react";
+import { MarkerSeverity } from "monaco-editor/esm/vs/editor/editor.api";
 import { useEffect } from "react";
 
 interface CodeEditorProps {
@@ -7,6 +8,7 @@ interface CodeEditorProps {
 
 export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 	const monaco = useMonaco();
+	const linterOwner = "redCodeLinter";
 
 	const testProgram = `; heheheha, code here :D
 
@@ -19,34 +21,49 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 
 	useEffect(() => {
 		if (monaco) {
-			setTimeout(setUpTestingLinterWarningsAndErrors, 500);
+			//TODO: just for testing rn, will be deleted
+			setUpTestingLinterWarningsAndErrors();
 		}
 	}, [monaco]);
 
 	function setUpTestingLinterWarningsAndErrors() {
+		setTimeout(
+			() =>
+				setLinterLinting(
+					MarkerSeverity.Error,
+					1,
+					1,
+					1,
+					10,
+					"This is an error1",
+				),
+			2000,
+		);
+	}
+
+	function setLinterLinting(
+		severity: MarkerSeverity,
+		startLineNumber: number,
+		endLineNumber: number,
+		startColumn: number,
+		endColumn: number,
+		message: string,
+	) {
 		if (!monaco) return;
 		const model = monaco.editor.getModels()[0];
-		const markers = [
-			{
-				severity: monaco.MarkerSeverity.Warning,
-				startLineNumber: 2,
-				startColumn: 1,
-				endLineNumber: 2,
-				endColumn: 50,
-				message: "whoopsies there is an wornin in line 2 >w<",
-			},
-			{
-				severity: monaco.MarkerSeverity.Error,
-				startLineNumber: 4,
-				startColumn: 1,
-				endLineNumber: 4,
-				endColumn: 50,
-				message: "whoopsies there is an ewwor in line 4 >w<",
-			},
-		];
-
-		// Set markers on the model
-		monaco.editor.setModelMarkers(model, "customLinter", markers);
+		const marker = {
+			severity: severity,
+			startLineNumber: startLineNumber,
+			startColumn: startColumn,
+			endLineNumber: endLineNumber,
+			endColumn: endColumn,
+			message: message,
+			owner: linterOwner,
+			resource: model.uri,
+		};
+		const markers = monaco.editor.getModelMarkers({ owner: linterOwner });
+		markers.push(marker);
+		monaco.editor.setModelMarkers(model, linterOwner, markers);
 	}
 
 	return (
