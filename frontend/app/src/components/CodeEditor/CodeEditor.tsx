578ba316b5eadc1cd1b3f8052b3b09ab45d2c68b
straczi <k.arendt0302@gmail.com>
2024-11-08 18:55:23 +0100

    [#180] add file upload / download to editor

--- frontend/app/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/app/src/components/CodeEditor/CodeEditor.tsx
@@ -3,13 +3,28 @@ import { Linterlint } from "@/domain/LinterLint";
 import { usePageVisibility } from "@/lib/usePageVisibility";
 import { getLinterLintsV1 } from "@/services/rest/RestService";
 import { Editor, useMonaco } from "@monaco-editor/react";
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { Button } from "../ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Input } from "../ui/input";
 
 interface CodeEditorProps {
 	setProgram: (s: string) => void;
+	program: string;
 }
 
-export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
+export default function CodeEditor({
+	setProgram,
+	program,
+}: Readonly<CodeEditorProps>) {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 	const monaco = useMonaco();
 	const linterOwner = "redCodeLinter";
 	const isPageVisible = usePageVisibility();
@@ -48,6 +63,8 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 		return () => {
 			stopPolling();
 		};
+		// eslint is disabled because adding setLinterLinting to the dependency array would cause an infinite loop
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isPageVisible, monaco]);
 
 	function setLinterLinting(linterLints: Linterlint[]) {
@@ -70,13 +87,77 @@ export default function CodeEditor({ setProgram }: Readonly<CodeEditorProps>) {
 		monaco.editor.setModelMarkers(model, linterOwner, markers);
 	}
 
+	async function getFileContentAndSetCode(e: ChangeEvent<HTMLInputElement>) {
+		const file = e.target.files?.[0];
+		if (file) {
+			const text = await file.text();
+			setProgram(text);
+		}
+		setIsDropdownOpen(false);
+	}
+
+	function downloadCode() {
+		setIsDropdownOpen(false);
+		const element = document.createElement("a");
+		const file = new Blob([program], { type: "text/plain" });
+		element.href = URL.createObjectURL(file);
+		element.download = "myCoreWarProgram.txt";
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
+	}
+
 	return (
-		<Editor
-			height="60vh"
-			theme="corewarTheme"
-			defaultLanguage="redcode"
-			onMount={(editor) => setProgram(editor.getValue())}
-			onChange={(value) => setProgram(value ?? "")}
-		/>
+		<>
+			<DropdownMenu open={isDropdownOpen}>
+				<DropdownMenuTrigger>
+					<div className="border-2 border-slate-900 inline-block">
+						<Button
+							variant="ghost"
+							className="mx-0.5 h-[20px] w-[30px]"
+							onClick={() => setIsDropdownOpen(true)}
+						>
+							File
+						</Button>
+					</div>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent
+					className=""
+					onEscapeKeyDown={() => setIsDropdownOpen(false)}
+					onPointerDownOutside={() => setIsDropdownOpen(false)}
+				>
+					<DropdownMenuLabel>File</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<label>
+							upload
+							<Input
+								type="file"
+								className="hidden w-[100%] h-[100%]"
+								onChange={getFileContentAndSetCode}
+							/>
+						</label>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<label>
+							download
+							<Button
+								onClick={downloadCode}
+								value="download"
+								className="hidden"
+							/>
+						</label>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			<Editor
+				height="60vh"
+				theme="corewarTheme"
+				defaultLanguage="redcode"
+				value={program}
+				onMount={(editor) => setProgram(editor.getValue())}
+				onChange={(value) => setProgram(value ?? "")}
+			/>
+		</>
 	);
 }
