d587e4933fed336636f7cfae081c5ae425ec5542
Say <sailsman@tutanota.com>
2024-11-10 21:23:48 +0100

    [#151] first userflow e2e test

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://0.0.0.0:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -43,10 +43,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-/*    {
+    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },*/
+    },
 
     /* Test against mobile viewports. */
     // {
--- e2e/tests/fullUserflow.spec.ts
+++ e2e/tests/fullUserflow.spec.ts
@@ -1,7 +1,47 @@
 import {expect, test} from "@playwright/test";
 
-test("complete userflow", async ({ page }) => {
-    await page.goto('/');
+test("complete userflow", async ({ browser }) => {
+    const playerAContext = await browser.newContext();
+    const playerAPage = await playerAContext.newPage();
 
-    expect(1).toBe(1);
+    const playerBContext = await browser.newContext();
+    const playerBPage = await playerBContext.newPage();
+
+    await playerAPage.goto("/");
+    await playerAPage.getByText("Play").click();
+
+    await playerAPage.locator('div').filter({ hasText: /^PLAYER APLAY$/ }).getByRole('button').click();
+
+    await playerAPage.getByRole("textbox").focus()
+    await playerAPage.locator('.view-lines').click();
+    await playerAPage.keyboard.type('ADD 1, 1');
+    await playerAPage.getByRole('button', { name: 'upload' }).click();
+    await playerAPage.getByRole('button', { name: 'Confirm' }).click();
+
+    await expect(playerAPage.getByText("Waiting for opponent...")).toBeVisible();
+
+    await playerBPage.goto("/");
+    await playerBPage.getByText("Play").click();
+
+    await playerBPage.locator('div').filter({ hasText: /^PLAYER BPLAY$/ }).getByRole('button').click();
+
+    await playerBPage.getByRole("textbox").focus()
+    await playerBPage.locator('.view-lines').click();
+    await playerBPage.keyboard.type('ADD 1, 1');
+    await playerBPage.getByRole('button', { name: 'upload' }).click();
+    await playerBPage.getByRole('button', { name: 'Confirm' }).click();
+
+    await expect(playerBPage.getByText("Waiting for opponent...")).toBeVisible();
+
+    await expect(playerAPage.getByText("Waiting for game result...")).toBeVisible();
+    await expect(playerBPage.getByText("Waiting for game result...")).toBeVisible();
+
+    await expect(playerAPage.getByText("Result: Player A vs Player B")).toBeVisible();
+    await expect(playerBPage.getByText("Result: Player A vs Player B")).toBeVisible();
+
+    const playAgainButton = playerAPage.getByRole('button', { name: 'Play again' });
+    await expect(playAgainButton).toBeVisible();
+    await playAgainButton.click();
+
+    await expect(playerAPage).toHaveURL(/.*player-selection/);
 });
