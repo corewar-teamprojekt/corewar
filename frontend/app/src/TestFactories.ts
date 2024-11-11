305b717d46a757d9180677b1e8aa75d7a11f0e2a
straczi <k.arendt0302@gmail.com>
2024-11-11 15:47:07 +0100

    [#202] move mockLobbies to TestFactory

--- frontend/app/src/TestFactories.ts
+++ frontend/app/src/TestFactories.ts
@@ -8,3 +8,58 @@ export function aLobby(): Lobby {
 export function anotherLobby(): Lobby {
 	return new Lobby(1, ["playerB"], GameState.NOT_STARTED);
 }
+
+export function mockLobbies(): Lobby[] {
+	return [
+		{
+			id: 1,
+			playersJoined: ["Alice"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 2,
+			playersJoined: ["Charlie", "Dave"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 3,
+			playersJoined: ["Frank"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 4,
+			playersJoined: ["Grace"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 5,
+			playersJoined: ["Ivan", "Judy"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 6,
+			playersJoined: ["Kevin"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 7,
+			playersJoined: ["Laura"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 8,
+			playersJoined: ["Mike"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 9,
+			playersJoined: ["Nina"],
+			gameState: GameState.NOT_STARTED,
+		},
+		{
+			id: 10,
+			playersJoined: ["Oscar"],
+			gameState: GameState.NOT_STARTED,
+		},
+	];
+}
