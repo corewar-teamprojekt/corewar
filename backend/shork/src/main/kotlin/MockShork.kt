53c0f3b7440c31dbd160e70c35e07d72f42ac999
flamion <contact@flamion.dev>
2024-11-13 17:22:25 +0100

    [#205] Update MockShork to conform to new IShork definition

--- backend/shork/src/main/kotlin/MockShork.kt
+++ backend/shork/src/main/kotlin/MockShork.kt
@@ -2,10 +2,13 @@ package software.shonk.interpreter
 
 /** This is just a fake interpreter, that lets the longer program win */
 class MockShork : IShork {
-    override fun run(settings: Settings, programs: Map<String, String>): String? {
-        return programs
-            .map { entry -> entry.key to entry.value.length }
-            .maxByOrNull { it.second }
-            ?.first
+    override fun run(settings: Settings, programs: Map<String, String>): GameResult {
+        val winner =
+            programs
+                .map { entry -> entry.key to entry.value.length }
+                .maxByOrNull { it.second }
+                ?.first
+
+        return GameResult(GameOutcome(winner, OutcomeKind.WIN), emptyList())
     }
 }
