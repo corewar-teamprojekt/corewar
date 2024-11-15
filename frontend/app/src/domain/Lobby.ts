801a81fcca2c008ebbf524c71921b850a8e378e1
straczi <k.arendt0302@gmail.com>
2024-11-15 19:09:43 +0100

    [#202] fix behavior of LobbySelectionPage to disable lobbies from shifting

--- frontend/app/src/domain/Lobby.ts
+++ frontend/app/src/domain/Lobby.ts
@@ -4,6 +4,7 @@ export class Lobby {
 	public id: number;
 	public playersJoined: string[];
 	public gameState: GameState;
+	public isDisabled: boolean | undefined;
 
 	constructor(id: number, playersJoined: string[], gameState: GameState) {
 		this.id = id;
