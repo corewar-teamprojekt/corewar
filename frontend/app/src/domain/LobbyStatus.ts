58754b9c423b97895eae2ac4350939de414139f5
straczi <k.arendt0302@gmail.com>
2024-11-19 22:21:53 +0100

    [#189] add new class LobbyRest and implement some endpoint-usages

---
+++ frontend/app/src/domain/LobbyStatus.ts
@@ -0,0 +1,10 @@
+import { GameState } from "./GameState";
+
+export interface LobbyStatus {
+	playerASubmitted: boolean;
+	playerBSubmitted: boolean;
+	gameState: GameState;
+	result: {
+		winner: "A" | "b" | "DRAW";
+	};
+}
