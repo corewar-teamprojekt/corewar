c7cf36a2613b8ed7fbf9eaa89d374f258cea84d4
straczi <k.arendt0302@gmail.com>
2024-11-21 21:44:44 +0100

    [#189] finish PlayerSelectionPage and implement its test

--- frontend/app/src/pages/playerSelection/PlayerSelection.tsx
+++ frontend/app/src/pages/playerSelection/PlayerSelection.tsx
@@ -14,16 +14,21 @@ import {
 	useUser,
 } from "@/services/userContext/UserContextHelpers.ts";
 import { useEffect, useState } from "react";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styles from "./PlayerSelection.module.css";
 
 function PlayerSelection() {
 	const navigate = useNavigate();
+
 	const userDispatcher = useDispatchUser();
+	const lobbyDispatch = useDispatchLobby();
+
 	const lobby = useLobby();
 	const user = useUser();
-	const lobbyDispatch = useDispatchLobby();
+
 	const [blockLogout, setBlockLogout] = useState<boolean>(false);
 	const [shouldRedirect, setShouldRedirect] = useState(false);
+
 	const { toast } = useToast();
 
 	useEffect(() => {
@@ -34,42 +39,48 @@ function PlayerSelection() {
 	}, [lobby, user, shouldRedirect, navigate]);
 
 	useEffect(() => {
-		function createAndJoinNewLobby() {
-			if (!user || !lobbyDispatch) {
+		async function joinLobbyWithPlayer(userName: string, lobbyId: number) {
+			if (!lobbyDispatch) {
 				return;
 			}
-			createLobby().then((lobbyId) => {
-				joinLobby(user.name, lobbyId)
-					.then(() => {
+			joinLobby(userName, lobbyId)
+				.catch(handleLobbyError)
+				.then(() => {
+					if (!lobby || lobby.id !== lobbyId) {
 						const newLobby = new Lobby(
 							lobbyId,
-							[user.name],
+							[userName],
 							GameState.NOT_STARTED,
 						);
 						lobbyDispatch({ type: "join", lobby: newLobby });
-						setShouldRedirect(true);
-					})
-					.catch(() => {
-						toast({
-							title: "Oopsie👉👈",
-							description: "Something went wrong while joining the lobby 😢",
-							variant: "destructive",
-						});
-						redirect("lobby-selection");
-					});
+					}
+					setShouldRedirect(true);
+				});
+		}
+
+		function handleLobbyError() {
+			toast({
+				title: "Oopsie👉👈",
+				description: "Something went wrong while joining the lobby 😢",
+				variant: "destructive",
 			});
+			if (userDispatcher) userDispatcher({ type: "logout", user: null });
+			navigate("/lobby-selection");
+			setBlockLogout(false);
 		}
 
 		if (!user || !lobbyDispatch) {
 			return;
 		} else if (lobby) {
-			joinLobby(user.name, lobby.id).then(() => {
-				setShouldRedirect(true);
-			});
+			joinLobbyWithPlayer(user.name, lobby.id);
 		} else {
-			createAndJoinNewLobby();
+			createLobby()
+				.catch(handleLobbyError)
+				.then((lobbyID) => {
+					if (lobbyID) joinLobbyWithPlayer(user.name, lobbyID);
+				});
 		}
-	}, [user, lobby, lobbyDispatch, toast]);
+	}, [user, lobby, lobbyDispatch, toast, userDispatcher, navigate]);
 
 	function setPlayer(actionType: "setPlayerA" | "setPlayerB") {
 		if (userDispatcher) {
@@ -81,22 +92,44 @@ function PlayerSelection() {
 		}
 	}
 
+	function isButtonDisabled(player: string): boolean {
+		return lobby?.playersJoined.includes(player) ?? false;
+	}
+
 	return (
 		<RequireLogout blocked={blockLogout}>
 			<div className="h-[100%] w-[100%] flex flex-col justify-center items-center ">
 				<div className="h-[100%] w-[100%] flex flex-row justify-center items-center gap-[12%]">
-					<button onClick={() => setPlayer("setPlayerA")}>
+					<button
+						onClick={() => setPlayer("setPlayerA")}
+						disabled={isButtonDisabled("playerA")}
+					>
 						<img
 							src={redPlayerIcon}
 							alt="Player A Icon"
-							className="w-164 h-164 transition-transform duration-300 hover:scale-110"
+							className={
+								styles[
+									isButtonDisabled("playerA")
+										? "player-icon-disabled"
+										: "player-icon"
+								]
+							}
 						/>
 					</button>
-					<button onClick={() => setPlayer("setPlayerB")}>
+					<button
+						onClick={() => setPlayer("setPlayerB")}
+						disabled={isButtonDisabled("playerB")}
+					>
 						<img
 							src={bluePlayerIcon}
 							alt="Player B Icon"
-							className="w-164 h-164 transition-transform duration-300 hover:scale-110"
+							className={
+								styles[
+									isButtonDisabled("playerB")
+										? "player-icon-disabled"
+										: "player-icon"
+								]
+							}
 						/>
 					</button>
 				</div>
