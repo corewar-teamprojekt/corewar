6152ac706e4766fdf5e6f4b479b4f6567809b27c
Say <sailsman@tutanota.com>
2024-11-22 22:36:17 +0100

    [#161] write doc comments for v0 endpoints, and re-generate corresponding openapi.yaml

--- backend/api/src/main/kotlin/software/shonk/adapters/incoming/V0ShorkInterpreterController.kt
+++ backend/api/src/main/kotlin/software/shonk/adapters/incoming/V0ShorkInterpreterController.kt
@@ -13,8 +13,28 @@ import software.shonk.application.port.incoming.V0ShorkUseCase
 fun Route.configureShorkInterpreterControllerV0() {
     val shorkUseCase by inject<V0ShorkUseCase>()
 
+    /**
+     * Returns either the status of the current game, or the status / result of the last game if
+     * there is no currently active game.
+     *
+     * playerXSubmitted will only be true if code for player X has been submitted
+     *
+     * result will only be valid when gameState is FINISHED
+     *
+     * playerXSubmitted will switch back to false once the gameState switches to FINISHED
+     */
     get("/status") { call.respond(shorkUseCase.getStatus()) }
 
+    /**
+     * Stores the program of a given player in current v0Lobby, creates that lobby if there is no
+     * current v0Lobby. <br> Path Parameter - {player}: The name of the player whose code is being
+     * submitted. For this endpoint ONLY 'playerA' and 'playerB' are valid! Body: The program of the
+     * player, in plain text <br> Response 200: The game will automatically start once the second
+     * player has submitted. Response 400: Game is in `RUNNING` state or a player still has to
+     * submit code. The message field contains an explanation of what went wrong.
+     *
+     * { "message": String, }
+     */
     post("/code/{player}") {
         val player = call.parameters["player"]
 
