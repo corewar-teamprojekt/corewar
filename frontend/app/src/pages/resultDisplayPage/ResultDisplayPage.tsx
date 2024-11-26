4c044901eb6ec33a8e4bfd5a016b99633febb530
straczi <k.arendt0302@gmail.com>
2024-11-26 20:55:22 +0100

    [#89] implement new tests for resultDisplayPageq

--- frontend/app/src/pages/resultDisplayPage/ResultDisplayPage.tsx
+++ frontend/app/src/pages/resultDisplayPage/ResultDisplayPage.tsx
@@ -3,23 +3,21 @@ import icon from "@/assets/icon.svg";
 import redPlayerIcon from "@/assets/redPlayerIcon.svg";
 import { Button } from "@/components/ui/button";
 import { LobbyStatus } from "@/domain/LobbyStatus";
-import { useLobby } from "@/services/lobbyContext/LobbyContextHelpers";
+import {
+	useDispatchLobby,
+	useLobby,
+} from "@/services/lobbyContext/LobbyContextHelpers";
 import { getLobbyStatusV1 } from "@/services/rest/LobbyRest";
 import { useUser } from "@/services/userContext/UserContextHelpers";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getLobbyStatusV1 } from "@/services/rest/LobbyRest.ts";
-import {
-	useDispatchLobby,
-	useLobby,
-} from "@/services/lobbyContext/LobbyContextHelpers.ts";
-import { LobbyStatus } from "@/domain/LobbyStatus.ts";
 
 export default function ResultDisplayPageV2() {
 	const [lobbyStatus, setLobbyStatus] = useState<LobbyStatus>();
 	const navigate = useNavigate();
 	const lobby = useLobby();
 	const user = useUser();
+	const lobbyDispatch = useDispatchLobby();
 
 	useEffect(() => {
 		if (lobby) {
@@ -31,9 +29,9 @@ export default function ResultDisplayPageV2() {
 		if (!lobby || !user || !lobbyStatus) {
 			return "Error";
 		}
-		if (lobbyStatus?.result.winner === "DRAW") {
+		if (lobbyStatus.result.winner === "DRAW") {
 			return "It's a draw!";
-		} else if (user?.name.slice(-1) === lobbyStatus?.result.winner) {
+		} else if (user.name.slice(-1) === lobbyStatus.result.winner) {
 			return "You won!";
 		} else {
 			return "You lost!";
@@ -42,16 +40,24 @@ export default function ResultDisplayPageV2() {
 
 	function getIconForWinner() {
 		if (
-			lobbyStatus?.result.winner === "DRAW" ||
+			!lobbyStatus ||
+			lobbyStatus.result.winner === "DRAW" ||
 			!lobby ||
-			!user ||
-			!lobbyStatus
+			!user
 		) {
-			return icon;
+			return <img src={icon} alt="draw icon" className={"w-164 h-164"} />;
 		} else if (lobbyStatus?.result.winner === "A") {
-			return redPlayerIcon;
+			return (
+				<img src={redPlayerIcon} alt="playerA icon" className={"w-164 h-164"} />
+			);
 		} else {
-			return bluePlayerIcon;
+			return (
+				<img
+					src={bluePlayerIcon}
+					alt="playerB icon"
+					className={"w-164 h-164"}
+				/>
+			);
 		}
 	}
 
@@ -59,32 +65,31 @@ export default function ResultDisplayPageV2() {
 		if (!lobby || !lobbyStatus || lobby.playersJoined.length < 2) {
 			return "";
 		}
-		let str = lobby.playersJoined[0];
+		let str = "Result: " + lobby.playersJoined[0];
 		for (let i = 1; i < lobby.playersJoined.length; i++) {
 			str += " vs " + lobby.playersJoined[i];
 		}
 		return str;
 	}
 
+	function onPlayAgainClick() {
+		if (lobbyDispatch) {
+			lobbyDispatch({ type: "leave", lobby: null });
+		}
+		navigate("/lobby-selection");
+	}
+
 	return (
 		<div>
 			<div className="flex flex-col justify-center items-center h-[100%] w-[100%] gap-10">
-				<h2 className="text-3xl font-semibold">
-					Result: {getAllPlayerVSString()}
-				</h2>
+				<h2 className="text-3xl font-semibold">{getAllPlayerVSString()}</h2>
 				<div className="flex flex-row items-center gap-[110px] justify-center">
 					<h2 className="text-7xl font-extrabold mb-[110px]">
 						{getResultText()}
 					</h2>
-					<img
-						src={getIconForWinner()}
-						alt="winner player icon"
-						className={"w-164 h-164"}
-					/>
+					{getIconForWinner()}
 				</div>
-				<Button onClick={() => navigate("/player-selection")}>
-					Play again
-				</Button>
+				<Button onClick={onPlayAgainClick}>Play again</Button>
 			</div>
 		</div>
 	);
