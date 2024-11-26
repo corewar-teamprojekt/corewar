b51d6ad0e083aa8469f8b17cb6505da921f3031d
Say <sailsman@tutanota.com>
2024-11-27 00:29:42 +0100

    [#270] use apiV1 getLobbyStatus endpoint in waitingForResult page

--- frontend/app/src/pages/waitingForResult/WaitingForResultPage.test.tsx
+++ frontend/app/src/pages/waitingForResult/WaitingForResultPage.test.tsx
@@ -6,6 +6,7 @@ import { createMemoryRouter, Navigate, RouterProvider } from "react-router-dom";
 import { useUser } from "@/services/userContext/UserContextHelpers";
 import { User } from "@/domain/User.ts";
 import { BASE_POLLING_INTERVAL_MS } from "@/consts.ts";
+import { aLobby } from "@/TestFactories.ts";
 
 const POLLING_BUFFER = 500;
 
@@ -31,13 +32,19 @@ beforeEach(() => {
 
 vi.mock("@/services/userContext/UserContextHelpers");
 
-// Might theoretically become flaky, since its working with actual timers
+// Might theoretically become flaky, since it's working with actual timers
 describe("backend polling", () => {
 	beforeEach(() =>
 		(useUser as Mock).mockReturnValue(new User("playerA", "#ffeeff")),
 	);
 
 	it("starts polling correct endpoint once component gets rendered", async () => {
+		const LOBBY_ID = 0;
+		vi.mock("@/services/lobbyContext/LobbyContextHelpers", () => ({
+			useDispatchLobby: vi.fn(),
+			useLobby: () => aLobby({ lobbyId: 0 }), // Lobby id from the top
+		}));
+
 		// Mock the fetch function
 		const mockFetch = vi.fn(() =>
 			Promise.resolve({
@@ -62,7 +69,7 @@ describe("backend polling", () => {
 		await waitFor(
 			() => {
 				expect(mockFetch).toHaveBeenCalledWith(
-					`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/v0/status`,
+					`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/v1/lobby/status/${LOBBY_ID}`,
 				);
 			},
 			{
