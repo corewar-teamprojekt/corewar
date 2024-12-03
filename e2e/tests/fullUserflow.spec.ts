3eed8160a2123304c71d9749f82fc464fb8898da
nikkitschierske <nikki@schnelle.dev>
2024-12-03 16:30:36 +0100

    [#300] extend playwright timeouts

--- e2e/tests/fullUserflow.spec.ts
+++ e2e/tests/fullUserflow.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("complete userflow", async ({ browser }) => {
     const playerAContext = await browser.newContext();
@@ -12,50 +12,62 @@ test("complete userflow", async ({ browser }) => {
     await playerAPage.getByText("Play").click();
 
     // Lobby selection page
-    await playerAPage.getByRole('button', { name: 'create lobby' }).click();
+    await playerAPage.getByRole("button", { name: "create lobby" }).click();
 
     // Player selection page
     await playerAPage.locator('button:has([alt="Player A Icon"])').click();
 
     // Player code input page
-    await playerAPage.getByRole("textbox").focus()
-    await playerAPage.locator('.view-lines').click();
-    await playerAPage.keyboard.type('ADD 1, 1');
-    await playerAPage.getByRole('button', { name: 'upload' }).click();
-    await playerAPage.getByRole('button', { name: 'Confirm' }).click();
+    await playerAPage.getByRole("textbox").focus();
+    await playerAPage.locator(".view-lines").click();
+    await playerAPage.keyboard.type("ADD 1, 1");
+    await playerAPage.getByRole("button", { name: "upload" }).click();
+    await playerAPage.getByRole("button", { name: "Confirm" }).click();
 
     // Waiting for opponent page
-    await expect(playerAPage.getByText("Waiting for opponent...")).toBeVisible();
+    await expect(playerAPage.getByText("Waiting for opponent..."))
+        .toBeVisible();
 
     // Landing page
     await playerBPage.goto("/");
     await playerBPage.getByText("Play").click();
 
     // Lobby selection page
-    await playerBPage.getByRole('button', { name: 'join' }).click();
+    await playerBPage.getByRole("button", { name: "join" }).click();
 
     // Player selection page
     await playerBPage.locator('button:has([alt="Player B Icon"])').click();
 
     // Player code input page
-    await playerBPage.getByRole("textbox").focus()
-    await playerBPage.locator('.view-lines').click();
-    await playerBPage.keyboard.type('ADD 1, 1');
-    await playerBPage.getByRole('button', { name: 'upload' }).click();
-    await playerBPage.getByRole('button', { name: 'Confirm' }).click();
+    await playerBPage.getByRole("textbox").focus();
+    await playerBPage.locator(".view-lines").click();
+    await playerBPage.keyboard.type("ADD 1, 1");
+    await playerBPage.getByRole("button", { name: "upload" }).click();
+    await playerBPage.getByRole("button", { name: "Confirm" }).click();
 
     // Waiting for opponent page
-    await expect(playerBPage.getByText("Waiting for opponent...")).toBeVisible();
+    await expect(playerBPage.getByText("Waiting for opponent...")).toBeVisible({
+        timeout: 60000,
+    });
 
-    await expect(playerAPage.getByText("Waiting for game result...")).toBeVisible();
-    await expect(playerBPage.getByText("Waiting for game result...")).toBeVisible();
+    await expect(playerAPage.getByText("Waiting for game result..."))
+        .toBeVisible({ timeout: 60000 });
+    await expect(playerBPage.getByText("Waiting for game result..."))
+        .toBeVisible({ timeout: 60000 });
 
-    await expect(playerAPage.getByText("It's a draw!")).toBeVisible();
-    await expect(playerBPage.getByText("It's a draw!")).toBeVisible();
+    await expect(playerAPage.getByText("It's a draw!")).toBeVisible({
+        timeout: 60000,
+    });
+    await expect(playerBPage.getByText("It's a draw!")).toBeVisible({
+        timeout: 60000,
+    });
 
-    const playAgainButton = playerAPage.getByRole('button', { name: 'Play again' });
+    const playAgainButton = playerAPage.getByRole("button", {
+        name: "Play again",
+    });
     await expect(playAgainButton).toBeVisible();
     await playAgainButton.click();
 
     await expect(playerAPage).toHaveURL(/.*lobby-selection/);
 });
+
