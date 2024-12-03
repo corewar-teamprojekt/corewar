649d86b152ffa686a7496c7305f0ca8d004eb062
flamion <contact@flamion.dev>
2024-12-03 21:04:50 +0100

    [#300] Adjust mockshork to also return game visualization data

--- backend/shork/src/main/kotlin/MockShork.kt
+++ backend/shork/src/main/kotlin/MockShork.kt
@@ -1,5 +1,7 @@
 package software.shonk.interpreter
 
+import software.shonk.interpreter.internal.statistics.RoundInformation
+
 /** This is just a fake interpreter, that lets the longer program win */
 class MockShork : IShork {
     override fun run(settings: Settings, programs: Map<String, String>): Result<GameResult> {
@@ -9,6 +11,21 @@ class MockShork : IShork {
                 .maxByOrNull { it.second }
                 ?.first
 
-        return Result.success(GameResult(GameOutcome(winner, OutcomeKind.WIN), emptyList()))
+        return Result.success(
+            GameResult(
+                GameOutcome(winner, OutcomeKind.WIN),
+                listOf(
+                    RoundInformation(
+                        playerId = "playerA",
+                        programCounterBefore = 0,
+                        programCounterAfter = 1,
+                        listOf(1, 2, 3),
+                        listOf(1, 2),
+                        listOf(42, 69),
+                        false,
+                    )
+                ),
+            )
+        )
     }
 }
