4fe5dd6d1498b7f04273286ddaa0cc4be021c5f9
Say <sailsman@tutanota.com>
2024-12-14 23:10:49 +0100

    [#294] change loading spinner to corewar icon

--- frontend/app/src/components/loadingSpinner/LoadingSpinner.tsx
+++ frontend/app/src/components/loadingSpinner/LoadingSpinner.tsx
@@ -2,23 +2,13 @@ import styles from "./LoadingSpinner.module.css";
 
 function LoadingSpinner() {
 	return (
-		<svg
-			width="300"
-			height="300"
-			viewBox="0 0 300 300"
-			xmlns="http://www.w3.org/2000/svg"
+		<img
+			width="600"
+			height="600"
 			className={styles.loadingSpinner}
-		>
-			<path
-				d="
-                M 150,10
-                A 140,140 0 1,1 10,150
-            "
-				stroke="white"
-				strokeWidth="20"
-				fill="none"
-			/>
-		</svg>
+			alt="Loading spinner"
+			src={"corewarIcon.svg"}
+		></img>
 	);
 }
 
